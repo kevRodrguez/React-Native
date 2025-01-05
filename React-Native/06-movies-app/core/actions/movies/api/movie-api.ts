bc84e15ff9d4a212aee7118c37ba858dd04bc966
Kevin Rodríguez <143865659+kevRodrguez@users.noreply.github.com>
2025-01-04 19:33:09 -0600

    Delete React-Native/06-movies-app directory

--- React-Native/06-movies-app/core/actions/movies/api/movie-api.ts
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-export const movieAPI = axios.create({
-   baseURL: process.env.EXPO_PUBLIC_MOVIE_DB_URL,
-   params: {
-        language: 'es-MX',
-        api_key: process.env.EXPO_PUBLIC_MOVIE_DB_KEY,
-   } 
-})
