ea47ea39a827aebb4ded1cc5a9d8ad20eb3265f8
Kevin Rodriguez <kevrodriguez@MacBook-Pro-de-Kevin.local>
2025-01-14 12:29:25 -0600

    commit integracion con mapps, error al generar el prebuild

--- React-Native/10-maps-app/app/map/index.tsx
+++ React-Native/10-maps-app/app/map/index.tsx
@@ -1,23 +1,46 @@
 import { View, Text, SafeAreaView } from 'react-native'
+import { StyleSheet } from 'react-native'
+import MapView,  {PROVIDER_GOOGLE}from 'react-native-maps'
 import React from 'react'
 
 const MapScreen = () => {
   return (
-    <SafeAreaView style={{
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center'
-      }}>
-      <View>
-        <Text style={{
-          textAlign: 'center',
-          fontSize: 20,
-          fontWeight: 'bold'
 
-        }}>MapScreen</Text>
-      </View>
-    </SafeAreaView>
+    <View style={styles.container}>
+      <MapView
+        style={styles.map}
+
+        //13.992722, -89.545335
+        //provider={PROVIDER_GOOGLE}
+        
+        initialRegion={{
+          latitude: 13.992722,
+          longitude: -89.545335,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+        // showsPointsOfInterest={false}
+      />
+    </View>
   )
 }
 
 export default MapScreen
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+
+  map: {
+    width: '100%',
+    height: '100%',
+    // backgroundColor: 'red',
+  },
+
+  text: {
+    textAlign: 'center',
+    fontSize: 20,
+    fontWeight: 'bold'
+  }
+});
